f447bb0fcadad70b7736a28c813bcf491df99102
Desiree <desireepoland@gmail.com>
2015-10-27 16:35:49 -0700

    Add images into public directory, add index, projects, about, and soon erb files to views directory, update my-site.rb file

--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,19 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
+  get "/index.html" do
+    erb :index
+  end
 
+  get "/soon.html" do
+    erb :soon
+  end
+
+  get "/projects.html" do
+    erb :projects
+  end
+
+  get "/about.html" do
+    erb :about
   end
 end
