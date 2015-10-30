8ed00dc538068d8f829225edea0a5808e8b579c3
Desiree <desireepoland@gmail.com>
2015-10-30 09:55:58 -0700

    Add styling

--- my-site.rb
+++ my-site.rb
@@ -3,22 +3,27 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @title = "Desiree Poland"
     erb :index
   end
 
   get "/index.html" do
+    @title = "Desiree Poland"
     erb :index
   end
 
-  get "/soon.html" do
-    erb :soon
-  end
-
   get "/projects.html" do
+    @title = "Projects"
     erb :projects
   end
 
   get "/about.html" do
+    @title = "About Desiree"
     erb :about
+  end
+
+  get "/soon.html" do
+    @title = "Coming Soon"
+    erb :soon
   end
 end
